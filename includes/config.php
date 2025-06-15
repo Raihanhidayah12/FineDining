b7d42cc54d9e1cee804992c35faa20135916c6f1
Raihanhidayah12 <139624351+Raihanhidayah12@users.noreply.github.com>
2025-06-15 14:24:57 +0000

    Added MySQLi extension and fixed config

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,9 @@
+
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db   = 'finedining';
-
-$conn = mysqli_connect($host, $user, $pass, $db);
-
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+$conn = mysqli_connect("localhost", "username", "password", "finedining");
+if ($conn) {
+    echo "Connected successfully!";
+} else {
+    echo "Connection failed: " . mysqli_connect_error();
 }
+?>
